937a99054249ebadeb070d4c6e07ed838517c3ce
EddyVerbruggen <eddyverbruggen@gmail.com>
2014-07-07 15:10:46 +0200

    [#7] Fix for (at least) the Nexus 5

--- src/android/nl/xservices/plugins/Flashlight.java
+++ src/android/nl/xservices/plugins/Flashlight.java
@@ -1,5 +1,6 @@
 package nl.xservices.plugins;
 
+import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.util.Log;
 import org.apache.cordova.CallbackContext;
@@ -14,29 +15,42 @@ public class Flashlight extends CordovaPlugin {
   private static final String ACTION_SWITCH_OFF = "switchOff";
 
   private static Boolean capable;
+  private boolean releasing;
   private Camera mCamera;
 
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     Log.d("Flashlight", "Plugin Called: " + action);
-    if (action.equals(ACTION_SWITCH_ON)) {
-      mCamera = Camera.open();
-      toggleTorch(true, callbackContext);
-      return true;
-    } else if (action.equals(ACTION_SWITCH_OFF)) {
-      toggleTorch(false, callbackContext);
-      releaseCamera();
-      return true;
-    } else if (action.equals(ACTION_AVAILABLE)) {
-      if (capable == null) {
+    try {
+      if (action.equals(ACTION_SWITCH_ON)) {
+        // When switching on immediately after checking for isAvailable,
+        // the release method may still be running, so wait a bit.
+        while (releasing) {
+          Thread.sleep(10);
+        }
         mCamera = Camera.open();
-        capable = isCapable();
+        // required for (at least) the Nexus 5
+        mCamera.setPreviewTexture(new SurfaceTexture(0));
+        toggleTorch(true, callbackContext);
+        return true;
+      } else if (action.equals(ACTION_SWITCH_OFF)) {
+        toggleTorch(false, callbackContext);
         releaseCamera();
+        return true;
+      } else if (action.equals(ACTION_AVAILABLE)) {
+        if (capable == null) {
+          mCamera = Camera.open();
+          capable = isCapable();
+          releaseCamera();
+        }
+        callbackContext.success(capable ? 1 : 0);
+        return true;
+      } else {
+        callbackContext.error("flashlight." + action + " is not a supported function.");
+        return false;
       }
-      callbackContext.success(capable ? 1 : 0);
-      return true;
-    } else {
-      callbackContext.error("flashlight." + action + " is not a supported function.");
+    } catch (Exception e) {
+      callbackContext.error(e.getMessage());
       return false;
     }
   }
@@ -60,12 +74,14 @@ public class Flashlight extends CordovaPlugin {
   }
 
   private void releaseCamera() {
+    releasing = true;
     // we need to release the camera, so other apps can use it
     new Thread(new Runnable() {
       public void run() {
         mCamera.setPreviewCallback(null);
         mCamera.stopPreview();
         mCamera.release();
+        releasing = false;
       }
     }).start();
   }
