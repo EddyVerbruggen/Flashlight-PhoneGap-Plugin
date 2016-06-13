1eafc72154a063a7929b36be6ae01dd7825a4611
EddyVerbruggen <eddyverbruggen@gmail.com>
2016-06-13 14:26:24 +0200

    #38 Android M compatibility (runtime camera permission)

--- src/android/nl/xservices/plugins/Flashlight.java
+++ src/android/nl/xservices/plugins/Flashlight.java
@@ -1,15 +1,21 @@
 package nl.xservices.plugins;
 
+import android.Manifest;
 import android.content.pm.FeatureInfo;
 import android.content.pm.PackageManager;
 import android.graphics.SurfaceTexture;
 import android.hardware.Camera;
 import android.os.Build;
 import android.util.Log;
+
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PermissionHelper;
+import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
+
+import java.io.IOException;
 
 public class Flashlight extends CordovaPlugin {
 
@@ -21,34 +27,38 @@ public class Flashlight extends CordovaPlugin {
   private boolean releasing;
   private Camera mCamera;
 
+  private static final int PERMISSION_CALLBACK_CAMERA = 33;
+  private String[] permissions = {Manifest.permission.CAMERA};
+  private CallbackContext callbackContext;
+
   @Override
   public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
     Log.d("Flashlight", "Plugin Called: " + action);
+
     try {
       if (action.equals(ACTION_SWITCH_ON)) {
+        this.callbackContext = callbackContext;
+
         // When switching on immediately after checking for isAvailable,
         // the release method may still be running, so wait a bit.
         while (releasing) {
           Thread.sleep(10);
         }
-        mCamera = Camera.open();
-        if (Build.VERSION.SDK_INT >= 11) { // honeycomb
-          // required for (at least) the Nexus 5
-          mCamera.setPreviewTexture(new SurfaceTexture(0));
+
+        // android M permission
+        if (!hasPermisssion()) {
+          requestPermissions(PERMISSION_CALLBACK_CAMERA);
+        } else {
+          toggleTorch(true, callbackContext);
         }
-        toggleTorch(true, callbackContext);
+
         return true;
       } else if (action.equals(ACTION_SWITCH_OFF)) {
         toggleTorch(false, callbackContext);
         releaseCamera();
         return true;
       } else if (action.equals(ACTION_AVAILABLE)) {
-        if (capable == null) {
-          mCamera = Camera.open();
-          capable = isCapable();
-          releaseCamera();
-        }
-        callbackContext.success(capable ? 1 : 0);
+        callbackContext.success(isCapable() ? 1 : 0);
         return true;
       } else {
         callbackContext.error("flashlight." + action + " is not a supported function.");
@@ -61,24 +71,75 @@ public class Flashlight extends CordovaPlugin {
   }
 
   private boolean isCapable() {
-    final PackageManager packageManager = this.cordova.getActivity().getPackageManager();
-    for (final FeatureInfo feature : packageManager.getSystemAvailableFeatures()) {
-      if (PackageManager.FEATURE_CAMERA_FLASH.equalsIgnoreCase(feature.name)) {
-        return true;
+    if (capable == null) {
+      capable = false;
+      final PackageManager packageManager = this.cordova.getActivity().getPackageManager();
+      for (final FeatureInfo feature : packageManager.getSystemAvailableFeatures()) {
+        if (PackageManager.FEATURE_CAMERA_FLASH.equalsIgnoreCase(feature.name)) {
+          capable = true;
+          break;
+        }
       }
     }
-    return false;
+    return capable;
   }
 
   private void toggleTorch(boolean switchOn, CallbackContext callbackContext) {
+    try {
+      if (isCapable()) {
+        doToggleTorch(switchOn, callbackContext);
+      } else {
+        callbackContext.error("Device is not capable of using the flashlight. Please test with flashlight.available()");
+      }
+    } catch (Exception e) {
+      callbackContext.error(e.getMessage());
+    }
+  }
+
+  @SuppressWarnings("deprecation")
+  private void doToggleTorch(boolean switchOn, CallbackContext callbackContext) throws IOException {
+    if (mCamera == null) {
+      mCamera = Camera.open();
+      if (Build.VERSION.SDK_INT >= 11) { // honeycomb
+        // required for (at least) the Nexus 5
+        mCamera.setPreviewTexture(new SurfaceTexture(0));
+      }
+    }
     final Camera.Parameters mParameters = mCamera.getParameters();
-    if (isCapable()) {
-      mParameters.setFlashMode(switchOn ? Camera.Parameters.FLASH_MODE_TORCH : Camera.Parameters.FLASH_MODE_OFF);
-      mCamera.setParameters(mParameters);
-      mCamera.startPreview();
-      callbackContext.success();
-    } else {
-      callbackContext.error("Device is not capable of using the flashlight. Please test with flashlight.available()");
+    mParameters.setFlashMode(switchOn ? Camera.Parameters.FLASH_MODE_TORCH : Camera.Parameters.FLASH_MODE_OFF);
+    mCamera.setParameters(mParameters);
+    mCamera.startPreview();
+    callbackContext.success();
+  }
+
+  public boolean hasPermisssion() {
+    for (final String p : permissions) {
+      if (!PermissionHelper.hasPermission(this, p)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public void requestPermissions(int requestCode) {
+    PermissionHelper.requestPermissions(this, requestCode, permissions);
+  }
+
+  public void onRequestPermissionResult(int requestCode, String[] permissions,
+                                        int[] grantResults) throws JSONException {
+    PluginResult result;
+    for (int r : grantResults) {
+      if (r == PackageManager.PERMISSION_DENIED) {
+        result = new PluginResult(PluginResult.Status.ILLEGAL_ACCESS_EXCEPTION);
+        this.callbackContext.sendPluginResult(result);
+        return;
+      }
+    }
+
+    switch (requestCode) {
+      case PERMISSION_CALLBACK_CAMERA:
+        toggleTorch(true, callbackContext);
+        break;
     }
   }
 
@@ -87,9 +148,13 @@ public class Flashlight extends CordovaPlugin {
     // we need to release the camera, so other apps can use it
     new Thread(new Runnable() {
       public void run() {
-        mCamera.setPreviewCallback(null);
-        mCamera.stopPreview();
-        mCamera.release();
+        if (mCamera != null) {
+          mCamera.stopPreview();
+          mCamera.setPreviewCallback(null);
+          mCamera.unlock();
+          mCamera.release();
+          mCamera = null;
+        }
         releasing = false;
       }
     }).start();
